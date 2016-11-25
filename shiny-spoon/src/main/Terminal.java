04ceffb57a904c0e88318545e95d52b893cea74d
xivv <saverio_mancuso@gmx.de>
2016-11-25 17:50:33 +0100

    Update Terminal.java

--- shiny-spoon/src/main/Terminal.java
+++ shiny-spoon/src/main/Terminal.java
@@ -5,7 +5,7 @@ public class Terminal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello Git");
+		System.out.println("Hello I changed Git");
 	}
 
 }
